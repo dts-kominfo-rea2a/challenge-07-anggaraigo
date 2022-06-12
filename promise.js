e797b9f55875588264ad15616ced4bb0fba1ddf5
igo <igoanggaraa@gmail.com>
2022-06-12 22:02:41 +0700

    Tantangan: Promise

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const Tixx = await promiseTheaterIXX();
+    const Tvgc = await promiseTheaterVGC();
+
+    const dataFilm = Tixx.concat(Tvgc);
+    return dataFilm.filter((Count) => Count.hasil == emosi).length;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = {
   promiseOutput,
